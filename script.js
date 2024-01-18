e9d23bfc6bc03f613e4cdf83127a824a79c0fdfb
IT21207754 <86506992+Isuru-rangana@users.noreply.github.com>
2024-01-18 21:22:39 +0530

    add chart

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const spinBtn = document.getElementById('spin-btn');
 const finalValue =document.getElementById('final-value');
 
 
-const rotationValues=[{minDegree:0,maxDegree:30,value:2},
+const rotationValues=[
+    {minDegree:0,maxDegree:30,value:2},
     {minDegree:31,maxDegree:90,value:1},
     {minDegree:91,maxDegree:150,value:6},
     {minDegree:151,maxDegree:210,value:5},
@@ -11,3 +12,39 @@ const rotationValues=[{minDegree:0,maxDegree:30,value:2},
     {minDegree:271,maxDegree:330,value:3},
     {minDegree:331,maxDegree:360,value:2},
 ];
+
+//size of each piece
+const data=[16,16,16,16,16,16];
+
+//backgrounfd color for each piece
+
+var pieColors=
+["green", "yellow", "red", "black", "blue","pink"];
+
+
+//create chart
+
+let myChart=new Chart(wheel,{
+    //plugin for displaying text on pie chart
+
+    Plugins:[ChartDataLabels],
+
+    type: "pie",
+    data:{
+        //labels(values which are to be displayed on chart)
+        labels:[1,2,3,4,5,6],
+
+
+        //setting for dataset/pie
+        datasets:[{
+
+            backgroundColor: pieColors,
+            data:data
+
+    },
+],
+
+    },
+
+    Options:{}
+});
